a439d914f9d5e94e48facfedaabe47b462aa6811
Serhii Zashchelkin <serg.zz@ukr.net>
2023-11-28 14:37:28 +0100

    Moved ServerConfig to lib

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env::args;
 use std::fs;
 use std::io::{Error, ErrorKind};
 use std::num::ParseIntError;
-use qs3_lib::client::qsend;
+use qs3_lib::client::ServerConfig;
 use qs3_lib::load_key_file;
 use qs3_lib::network::{QHandler, qserver};
 use s3cli_lib::{build_key_info, KeyInfo, S3KeyInfo};
@@ -20,35 +20,6 @@ line1: host:port
 line2: s3 secret
 
 */
-
-struct ServerConfig {
-    host: String,
-    s3_secret: String
-}
-
-impl ServerConfig {
-    fn new(data: Vec<u8>) -> Result<ServerConfig, Error> {
-        let text =
-            String::from_utf8(data).map_err(|e| Error::new(ErrorKind::InvalidData, e.to_string()))?;
-        let lines: Vec<String> = text
-            .split('\n')
-            .map(|v| v.to_string().trim().to_string())
-            .collect();
-        if lines.len() < 2 {
-            return Err(Error::new(ErrorKind::InvalidData, "invalid server configuration file"));
-        }
-        Ok(ServerConfig{ host: lines[0].trim().to_string(), s3_secret: lines[1].trim().to_string()})
-    }
-
-    fn build_request(&self, method: u8, file_name: &String) -> Vec<u8> {
-        let mut request = Vec::new();
-        request.push(method);
-        request.push(file_name.len() as u8);
-        request.extend_from_slice(file_name.as_bytes());
-        request.extend_from_slice(self.s3_secret.as_bytes());
-        request
-    }
-}
 
 struct RequestInfo {
     method: String,
@@ -142,8 +113,7 @@ fn send_request_to_server(rsa_key: String, arguments: Vec<String>) -> Result<(),
         _ => return Err(Error::new(ErrorKind::InvalidInput, "invalid operation, only get or put are allowed"))
     };
     let server_config = ServerConfig::new(fs::read(&arguments[2])?)?;
-    let data = server_config.build_request(method, &arguments[4]);
-    let response = qsend(rsa_key.as_str(), &server_config.host, data, 2, 3)?;
+    let response = server_config.qsend(rsa_key.as_str(), method, &arguments[4], 2, 3)?;
     let url = String::from_utf8(response)
         .map_err(|_e|Error::new(ErrorKind::InvalidData, "incorrect response from server"))?;
     println!("{}", url);
